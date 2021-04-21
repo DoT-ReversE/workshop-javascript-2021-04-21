2cf6fe8e04ed073bd72597f675a9cd29e57c45d8
Chanon Chokethaworn <chanon.cho@kbtg.tech>
2021-04-21 14:07:25 +0700

    Create User component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import User from './components/User';
 
 function App() {
   return (
-    <div className="App">
-      Hello React ...
+    <div>
+      <ul>
+        <User info='User 01'/>
+        <User info='User 02'/>
+        <User info='User 03'/>
+      </ul>
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function User( {info} ) {
+    return (
+        <li>{info}</li>
+    );
+}
+
+export default User;
